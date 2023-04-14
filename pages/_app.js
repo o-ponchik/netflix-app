a7bb75febf4533404e39c243d4b683ec1e813ff0
Maria Matviian <matviyanm@gmail.com>
2023-04-14 16:44:16 -0400

    cleanup code, import styles and font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@components/styles/globals.css'
+import "@components/styles/globals.css";
+import { Roboto_Slab } from "next/font/google";
+
+const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={robotoSlab.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
