2a169a6fb7f486b2303addc10a2b32d250455369
Maria Matviian <matviyanm@gmail.com>
2023-04-17 10:32:34 -0400

    Style Banner component

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "./banner.module.css";
+import Image from "next/image";
 
 const Banner = (props) => {
   const { title, subTitle, imgUrl } = props;
@@ -7,10 +9,28 @@ const Banner = (props) => {
     console.log("handleOnPlay");
   };
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <Image
+                src="/static/play_arrow_icon.svg"
+                alt="Play icon"
+                width="32"
+                height="32"
+              />
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
         style={{
           backgroundImage: `url(${imgUrl})`,
