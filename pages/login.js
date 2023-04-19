f801d0618c3beb72ed68c30abb5a62f6a1ada596
Maria Matviian <matviyanm@gmail.com>
2023-04-19 10:58:46 -0400

    add a structure and styles to the SignIn Page

--- pages/login.js
+++ pages/login.js
@@ -5,14 +5,19 @@ import Image from "next/image";
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+  const handleSignInWithEmail = (e) => {
+    e.preventDefault();
+    console.log("Hi");
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Netflix SignIn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
+      <header className={styles.header}>
         <div className={styles.headerWrapper}>
           <Link href="/" className={styles.logoLink}>
             <div className={styles.logoWrapper}>
@@ -27,6 +32,24 @@ const Login = () => {
           </Link>
         </div>
       </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Email address"
+            className={styles.emailInput}
+          />
+
+          <p className={styles.userMsg}></p>
+          <button onClick={handleSignInWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
