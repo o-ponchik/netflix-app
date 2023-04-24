65d593c30d65b17ab8b2a09a59d79818997a8137
Maria Matviian <matviyanm@gmail.com>
2023-04-24 14:39:42 -0400

    install and style Modal Component

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,27 @@
 import React from "react";
+import styles from "../../styles/Video.module.css";
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+
+Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
   console.log({ router });
 
-  return <div>Video</div>;
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        overlayClassName={styles.overlay}
+        className={styles.modal}
+      >
+        <div>Modal Body</div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Video;
